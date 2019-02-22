051f50e70cd9682ba4ef0de119896e65d541d873
lpcinelli <lpcinelli@gmail.com>
2019-02-22 20:11:49 -0300

    Adds dropout model and adds docs

--- models.py
+++ models.py
@@ -7,6 +7,94 @@ from torch.nn.parameter import Parameter
 from .metrics import kl_gaussian, stoch_kl_gaussian
 from torch.distributions import Normal
 
+
+def select_act(act_func):
+    if act_func == "relu":
+        return F.relu
+    if act_func == "tanh":
+        return F.tanh
+    if act_func == "sigmoid":
+        return F.sigmoid
+    raise ValueError("Invalid activation func. (relu, tanh, sigmoid).")
+
+
+##############################
+## Dropout Bayesian Network ##
+##############################
+
+
+class DropoutMLP(nn.Module):
+    """Implements "Dropout as as a Bayesian Approximation": before every weight
+    layer insert a dropout layer. If using Bernoulli dropout, the underlaying
+    variational distributionadgb
+
+    All weight Weights are initialized by the default rule for torch.nn.Linear (torch.nn.init.kaiming_uniform_):
+        W ~ U[ -1/fan_in, 1/fan_in ];
+
+    Arguments:
+        nn {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
+
+    def __init__(
+        self,
+        input_size,
+        hidden_sizes,
+        output_size,
+        drop_prob,
+        prior_prec,
+        act_func="relu",
+    ):
+        super(MLP, self).__init__()
+        self.input_size = input_size
+        self.hidden_sizes = hidden_sizes
+        self.drop_prob = drop_prob
+
+        # prior length-scale l -> p(W) ~ N(0, 1/l^2 * I) -> l = 1/sigma
+        # reg = len_scale**2 * (1 - drop_prob) / (2. * N * tau)
+
+        if output_size is not None:
+            self.output_size = output_size
+            self.squeeze_output = False
+        else:
+            self.output_size = 1
+            self.squeeze_output = True
+
+        # Set activation function
+        self.act = select_act(act_func)
+
+        # Define layers
+        if len(hidden_sizes) == 0:
+            # Linear model
+            self.hidden_layers = nn.ModuleList[nn.Dropout(self.drop_prob)]
+            self.output_layer = nn.Linear(self.input_size, self.output_size)
+        else:
+            # Neural network
+            hidden_layers = [
+                [nn.Dropout(self.drop_prob), nn.Linear(in_size, out_size)]
+                for in_size, out_size in zip(
+                    [self.input_size] + hidden_sizes[:-1], hidden_sizes
+                )
+            ] + [[nn.Dropout(self.drop_prob)]]
+            self.hidden_layers = nn.ModuleList(
+                [single_layer for item in hidden_layers for single_layer in item]
+            )
+            self.output_layer = nn.Linear(hidden_sizes[-1], self.output_size)
+
+    def forward(self, x):
+        # vectorize input samples
+        x = x.view(-1, self.input_size)
+        out = x
+        for layer in self.hidden_layers:
+            out = self.act(layer(out))
+        z = self.output_layer(out)
+        if self.squeeze_output:
+            z = torch.squeeze(z).view([-1])
+        return z
+
+
 ############################
 ## Multi-Layer Perceptron ##
 ############################
@@ -25,12 +113,7 @@ class MLP(nn.Module):
             self.squeeze_output = True
 
         # Set activation function
-        if act_func == "relu":
-            self.act = F.relu
-        elif act_func == "tanh":
-            self.act = F.tanh
-        elif act_func == "sigmoid":
-            self.act = F.sigmoid
+        self.act = select_act(act_func)
 
         # Define layers
         if len(hidden_sizes) == 0:
@@ -88,12 +171,7 @@ class BNN(nn.Module):
             self.squeeze_output = True
 
         # Set activation function
-        if act_func == "relu":
-            self.act = F.relu
-        elif act_func == "tanh":
-            self.act = F.tanh
-        elif act_func == "sigmoid":
-            self.act = F.sigmoid
+        self.act = select_act(act_func)
 
         # Define layers
         if len(hidden_sizes) == 0:
@@ -191,10 +269,20 @@ class StochasticLinear(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self):
-        # Init rule (for the mean): W ~ U[ -1/fan_in, 1/fan_in ]
-        stdv = 1. / math.sqrt(self.weight_mu.size(1))
+        """ Init rule for the mean (torch.nn.init.kaiming_uniform_ as in
+        torch.nn.Linear): W ~ U[ -1/fan_in, 1/fan_in ];
 
-        # Why not directly initialize weights_mu = 0 ; weight_spsigma = stdv ?
+        Using this initialization scheme (normal r.v. w/ uniform mean) the
+        resulting distribution is Gaussian if `stdv' <= `sigma_init`, else it
+        gets flat on top as a plateau. The variance of resulting random
+        variable is :math: `var(Uni) + var(Normal) = 1/(3*fan_in^2) +
+        sigma_init^2`. As `prec_init` is to 10 and :math: `sigma_init^2 =
+        1.0/prec_init = 0.1`, it dominates over the `fan_in` term and the final
+        r.v. is Gaussian. Hence, we have a Gaussian init :math: `N(0,
+        1/prec_init)`.
+        """
+        stdv = 1.0 / math.sqrt(self.weight_mu.size(1))
+
         self.weight_mu.data.uniform_(-stdv, stdv)
 
         # rho = log(exp(sigma) - 1) -> inverse of softplus transform
@@ -232,40 +320,42 @@ class StochasticLinear(nn.Module):
         return F.linear(input, self.weight, self.bias)
 
     def kl_divergence(self):
-        '''Compute the analytical KL divergence between current distribution and
+        """Compute the analytical KL divergence between current distribution and
         the prior. We assume the prior is a (multivariate) standard gaussian.
 
         Returns:
             [type] -- [description]
-        '''
+        """
 
         kl = kl_gaussian(
-                q_mu=self.weight_mu,
-                q_sigma=F.softplus(self.weight_spsigma),
-                p_mu=torch.zeros_like(self.weight_mu),
-                p_sigma=torch.ones_like(self.weight_spsigma) * self.sigma_prior
+            q_mu=self.weight_mu,
+            q_sigma=F.softplus(self.weight_spsigma),
+            p_mu=torch.zeros_like(self.weight_mu),
+            p_sigma=torch.ones_like(self.weight_spsigma) * self.sigma_prior,
         )
         if self.bias is not None:
             kl += kl_gaussian(
-                    q_mu=self.bias_mu,
-                    q_sigma=F.softplus(self.bias_spsigma),
-                    p_mu=torch.zeros_like(self.bias_mu),
-                    p_sigma=torch.ones_like(self.bias_spsigma) * self.sigma_prior
-                )
+                q_mu=self.bias_mu,
+                q_sigma=F.softplus(self.bias_spsigma),
+                p_mu=torch.zeros_like(self.bias_mu),
+                p_sigma=torch.ones_like(self.bias_spsigma) * self.sigma_prior,
+            )
         return kl
 
     def stoch_kl_divergence(self):
-        '''Compute the analytical KL divergence between current distribution and
+        """Compute the analytical KL divergence between current distribution and
         the prior. We assume the prior is a (multivariate) standard gaussian.
 
         Returns:
             [type] -- [description]
-        '''
+        """
 
         mu = torch.zeros_like(self.weight_mu)
         sigma = torch.ones_like(self.weight_spsigma) * self.sigma_prior
 
-        log_prob_qw = Normal(self.weight_mu, F.softplus(self.weight_spsigma)).log_prob(self.weight)
+        log_prob_qw = Normal(self.weight_mu, F.softplus(self.weight_spsigma)).log_prob(
+            self.weight
+        )
         log_prob_pw = Normal(mu, sigma).log_prob(self.weight)
 
         kl_approx = log_prob_qw.sum() - log_prob_pw.sum()
@@ -279,7 +369,9 @@ class StochasticLinear(nn.Module):
         if self.bias is not None:
             mu = torch.zeros_like(self.bias_mu)
             sigma = torch.ones_like(self.bias_spsigma) * self.sigma_prior
-            log_prob_qb = Normal(self.bias_mu, F.softplus(self.bias_spsigma)).log_prob(self.bias)
+            log_prob_qb = Normal(self.bias_mu, F.softplus(self.bias_spsigma)).log_prob(
+                self.bias
+            )
             log_prob_pb = Normal(mu, sigma).log_prob(self.bias)
             kl_approx += log_prob_qb.sum() - log_prob_pb.sum()
             # kl_approx += stoch_kl_gaussian(samples=self.bias,
@@ -318,12 +410,7 @@ class IndividualGradientMLP(nn.Module):
             self.squeeze_output = True
 
         # Set activation function
-        if act_func == "relu":
-            self.act = F.relu
-        elif act_func == "tanh":
-            self.act = F.tanh
-        elif act_func == "sigmoid":
-            self.act = F.sigmoid
+        self.act = select_act(act_func)
 
         # Define layers
         if len(hidden_sizes) == 0:
