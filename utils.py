27eb0d631960a354c133826a21c194a4a20485ab
lpcinelli <lpcinelli@gmail.com>
2019-02-25 22:31:47 -0300

    Adds class for accumulating avg in batches

--- utils.py
+++ utils.py
@@ -62,3 +62,22 @@ def add_weight_decay(net, l2_value, skip=()):
         {"params": no_decay, "weight_decay": 0.0},
         {"params": decay, "weight_decay": l2_value},
     ]
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
