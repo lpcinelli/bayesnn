ca3885be91d35d045a69648d8aad10ec1f42f8ac
lpcinelli <lpcinelli@gmail.com>
2019-11-10 22:40:29 -0300

    alter input to toy dataset class constructor

--- data_classes/regression/toy_data1d.py
+++ data_classes/regression/toy_data1d.py
@@ -12,24 +12,18 @@ class ToyData1D(data.Dataset):
                  data_set=None,
                  train=True,
                  x_points=[],
-                 base=None,
-                 noise=None):
+                 y_points=[]):
+        #  base=None,
+        #  noise=None):
         self.root = os.path.expanduser(root)
         self.train = train  # training set or test set
-        self.base_model = base
-        self.noise_model = noise
 
         if self.train:
-            self.train_data, self.train_labels = torch.FloatTensor(
-                x_points[0]), torch.FloatTensor(
-                    self.sample_data(x_points[0]).reshape(-1))
+            self.train_data = torch.FloatTensor(x_points[0]).view(-1, 1)
+            self.train_labels = torch.FloatTensor(y_points[0]).view(-1)
         else:
-            self.test_data, self.test_labels = torch.FloatTensor(
-                x_points[1]), torch.FloatTensor(
-                    self.sample_data(x_points[1]).reshape(-1))
-
-    def sample_data(self, x):
-        return self.base_model(x) + np.random.normal(0, self.noise_model(x))
+            self.test_data = torch.FloatTensor(x_points[1]).view(-1, 1)
+            self.test_labels = torch.FloatTensor(y_points[1]).view(-1)
 
     def __getitem__(self, index):
         """
