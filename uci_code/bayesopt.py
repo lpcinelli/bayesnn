8f63aa312c96e095d6e79c2b3cee8bf2a9f3633f
lpcinelli <lpcinelli@gmail.com>
2019-11-11 12:31:05 -0300

    Train mcdo 10x longer than others as orig paper

--- uci_code/bayesopt.py
+++ uci_code/bayesopt.py
@@ -242,6 +242,7 @@ def run_bayesopt(
 
     elif method == "dropout":
         model_params_final["dropout"] = bo.max["params"]["dropout"]
+        train_params.update({"num_epochs": 10*train_params["num_epochs"]})
         experiment = ExperimentDropoutMLPReg(
             results_folder=results_folder,
             data_folder=data_folder,
